7e3466795b422fd4105b6f54bf58bf6b3e26b59b
Vladislav <azzazzelko@gmail.com>
2017-01-13 17:46:05 +0300

    pages --> actions, blog, projects, designers (without some modals)

--- app/js/main.js
+++ app/js/main.js
@@ -2,24 +2,23 @@
 ;(function () {
   $(document).on('ready',function() {
 
-
     $("a.p-project").click(function() {
-        $('.layout').fadeIn('fast');
-        $('.projectSlider').slick('slickNext');
+      $('.layout').fadeIn('fast');
+      $('.projectSlider').slick('slickNext');
     });
 
     $("a.p-project").fancybox({
-        maxWidth	: 800,
-        minHeight   : 250,
-        fitToView	: false,
-        autoDimensions: 	true,
-        autoResize: true,
-        closeClick	: false,
-        openEffect	: 'fade',
-        closeEffect	: 'fade',
-        beforeClose: function() {
-            $('.layout').fadeOut('fast');
-        }
+      maxWidth	: 800,
+      minHeight   : 250,
+      fitToView	: false,
+      autoDimensions: 	true,
+      autoResize: true,
+      closeClick	: false,
+      openEffect	: 'fade',
+      closeEffect	: 'fade',
+      beforeClose: function() {
+        $('.layout').fadeOut('fast');
+      }
     });
 
     var projectsSlider = {
@@ -58,39 +57,39 @@
       fade: true,
       asNavFor: '.projectSlider'
     });
-    $('.projectSlider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        asNavFor: '.big_img',
-        focusOnSelect: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    dots: false,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    dots: false
-                }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1,
-                  dots: true
-              }
-            }
-        ]
-    });
 
+    $('.projectSlider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      asNavFor: '.big_img',
+      focusOnSelect: true,
+      responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          dots: false,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      }
+      ]
+    });
 
     $('.navigation li').hover(function () {
       if($(this).next().hasClass('submenu-block')) {
@@ -106,42 +105,25 @@
       $(this).removeClass('active');
     });
 
-    /*$('.m-menu > .burger-menu').click(function () {
-      $('body').css('overflow', 'hidden');
-      $('.layout').addClass('m-layout').fadeIn(300);
-      $('.close-menu').fadeIn(300);
-      $('.m-menu-wrapper').animate({left: "-230px"}, 300);
-    });
-
-    $('.m-menu-list li').click(function() {
-        if($(this).next().hasClass('m-submenu-block')) {
-            $(this).next().addClass('active');
-            $('.m-menu-list').animate({left: "230px"}, 300).addClass('level-2');
-        }
-    });
-
-    $(document).on('click', '.close-menu, .m-layout', function () {
-        $('.close-menu').fadeOut(300);
-        $('.layout').fadeOut(300);
-        $('body').css('overflow', 'auto');
-        $('.m-menu-wrapper').animate({left: "-460px"}, 300);
-        if($('.m-menu-list').hasClass('level-2')) {
-          $('.m-submenu-block').removeClass('active');
-          $('.m-menu-list').animate({left: "0"}, 300).removeClass('level-2');
-        }
-    });*/
-
     $('.m-menu').click(function () {
-        $('.m-menu-wrapper').slideToggle();
+      $('.m-menu-wrapper').slideToggle();
     })
 
     $('.m-menu-list li').click(function() {
-        if($(this).next().hasClass('active')) {
-            $(this).next().removeClass('active').slideUp();
-        } else {
-          $(this).next().addClass('active').slideDown();
-        }
+      if($(this).next().hasClass('active')) {
+        $(this).next().removeClass('active').slideUp();
+      } else {
+        $(this).next().addClass('active').slideDown();
+      }
     });
 
+
+    /* Slider */
+    if ( $("#slider").length ){
+      $("#slider").slider({
+        orientation: "horizontal",
+        range: "min"
+      });
+    }
   })
 })();
